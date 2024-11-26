2a862fd43bbf22373214775d94e0ba2184681fe8
anant parashar <anant.parashar@idemia.com>
2024-11-26 14:59:37 +0530

    Changes for APIs

--- apps/klavemyapp/types.ts
+++ apps/klavemyapp/types.ts
@@ -27,3 +27,50 @@ export class StoreInput {
 export class StoreOutput {
     success!: boolean;
 }
+
+@serializable
+export class SecureElementKey {
+    key!: string;
+}
+
+@serializable
+export class SecureElement {
+    key!: string;
+    field1!: string;
+    field2!: string;
+    creationDate!: i64;
+    status!: string;
+}
+
+@serializable
+export class SecureElementOutput {
+    success!: boolean;
+    secureElement!: string;
+}
+
+@serializable
+export class SecureElementOutputList {
+    success!: boolean;
+    seList!: SecureElement[];
+}
+
+@serializable
+export class Transac {
+    origin!: string;
+    payer!: string;
+    payee!: string;
+    amount: number = 0;
+    storeDate!: i64;
+}
+
+@serializable
+export class SecureElementTransaction {
+    secureElement!: string;
+    transactionList!: Transac[];
+}
+
+@serializable
+export class TransactionListOutput {
+    success!: boolean;
+    transactionList!: Transac[];
+}
